1c933bb9de74c486e9c7161f4225f5e4c9f0b1f5
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-05-23 02:35:01 +0800

    Pseudocode (2)

--- tv.py
+++ tv.py
@@ -1,1 +1,20 @@
 #Add Pseudocode
+
+#Create TV class
+#Define the TV class
+# Initialize the TV with default values
+
+# Create method to turn on the TV
+# Create method to turn off the TV
+
+# Create method to get the current channel
+# Create method to set the channel within the range of 1 to 120
+# Create method to increase the channel by 1, if the TV is on and the channel is less than 120
+# Create method to deccrease the channel by 1, if the TV is on and the channel is greater than 1
+
+# Create method to get the current volume level
+# Create method to set the volume level within the range of 1 to 7
+#Method to increase the volume by 1, if the TV is on and the volume is less than 7
+#Method to decrease the volume by 1, if the TV is on and the volume is greater than 1
+
+
