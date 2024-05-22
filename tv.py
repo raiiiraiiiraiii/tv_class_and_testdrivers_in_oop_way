4fc84a22b31d37748cda0d637435cde3afb101e6
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-05-23 02:44:29 +0800

    Pseudocode (4)

--- tv.py
+++ tv.py
@@ -10,11 +10,26 @@
 # Create method to get the current channel
 # Create method to set the channel within the range of 1 to 120
 # Create method to increase the channel by 1, if the TV is on and the channel is less than 120
-# Create method to deccrease the channel by 1, if the TV is on and the channel is greater than 1
+# Create method to decrease the channel by 1, if the TV is on and the channel is greater than 1
 
 # Create method to get the current volume level
 # Create method to set the volume level within the range of 1 to 7
 # Method to increase the volume by 1, if the TV is on and the volume is less than 7
 # Method to decrease the volume by 1, if the TV is on and the volume is greater than 1
 
+
+# Import TV class
+
+# A function to display the status of both TVs
+
+# A function to validate user's input
+
+# Display the options for controlling the TV
+# Get the user's choice and validate it
+# Perform the corresponding action based on the user's input
+
+# Create the two TV objects
+# Display the main options for the program
+# Get the user's choice and validate it
+# Perform the corresponding action based on the user'
 
