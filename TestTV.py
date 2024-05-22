de01e2eac7bcc6e6a6abd0beb3775bc332b6971d
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-05-23 02:48:29 +0800

    Pseudocode (5)

---
+++ TestTV.py
@@ -0,0 +1,14 @@
+# Import TV class
+
+# A function to display the status of both TVs
+
+# A function to validate user's input
+
+# Display the options for controlling the TV
+# Get the user's choice and validate it
+# Perform the corresponding action based on the user's input
+
+# Create the two TV objects
+# Display the main options for the program
+# Get the user's choice and validate it
+# Perform the corresponding action based on the user
--- tv.py
+++ tv.py
@@ -18,18 +18,5 @@
 # Method to decrease the volume by 1, if the TV is on and the volume is greater than 1
 
 
-# Import TV class
 
-# A function to display the status of both TVs
-
-# A function to validate user's input
-
-# Display the options for controlling the TV
-# Get the user's choice and validate it
-# Perform the corresponding action based on the user's input
-
-# Create the two TV objects
-# Display the main options for the program
-# Get the user's choice and validate it
-# Perform the corresponding action based on the user
 
