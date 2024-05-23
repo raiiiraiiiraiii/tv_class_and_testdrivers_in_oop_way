a10d93a870b972c88644c52607df530435e8249b
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-05-23 13:25:50 +0800

    Import TV

--- TestTV.py
+++ TestTV.py
@@ -1,14 +1,17 @@
 # Import TV class
-
-# A function to display the status of both TVs
+from TV import TV
 
 # A function to validate user's input
-
-# Display the options for controlling the TV
-# Get the user's choice and validate it
-# Perform the corresponding action based on the user's input
 
 # Create the two TV objects
 # Display the main options for the program
 # Get the user's choice and validate it
 # Perform the corresponding action based on the user
+
+# Display the options for controlling the TV
+# Get the user's choice and validate it
+# Perform the corresponding action based on the user's input
+
+
+
+# A function to display the status of both TVs
