e29c49f2292eee721fc64b67abfafde1936a3eb1
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-05-23 13:34:39 +0800

    Pseudocode (7)

--- TestTV.py
+++ TestTV.py
@@ -3,6 +3,8 @@ from TV import TV
 
 # Create the two TV objects
 
+# A function to display the status of both TVs
+
 # A function to validate user's input
 
 # Display the main options for the program
@@ -13,4 +15,3 @@ from TV import TV
 # Get the user's choice and validate it
 # Perform the corresponding action based on the user's input
 
-# A function to display the status of both TVs
