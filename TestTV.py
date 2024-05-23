2b64abf0003067cde6059bff75d1598620bbd69f
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-05-23 14:25:18 +0800

    Created Function to Validate Responses

--- TestTV.py
+++ TestTV.py
@@ -9,8 +9,18 @@ tv2 = TV()
 def show_status(tv1, tv2):
     print(f"\ntv1's channel is {tv1.getChannel()} and volume level is {tv1.getVolumeLevel()}")
     print(f"tv2's channel is {tv2.getChannel()} and volume level is {tv2.getVolumeLevel()}\n")
-    
+
 # A function to validate user's input
+def validate(prompt, valid_range = None):
+    while True:
+        try:
+            value = int(input(prompt))
+            if valid_range and value not in valid_range:
+                print(f"Please enter a value in the range {valid_range}.")
+            else:
+                return value
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
 
 # Display the main options for the program
 # Get the user's choice and validate it
