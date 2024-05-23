4f2a1634b526955cb8a8d3646f3978c5e0f89645
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-05-23 12:29:06 +0800

    Added Class Tv, define class TV, Initialize Values, Create method to turn on and off TV

--- tv.py
+++ tv.py
@@ -2,11 +2,20 @@
 
 # Create TV class
 # Define the TV class
+class TV:
+
 # Initialize the TV with default values
+    def __init__(self):
+        self.channel = 1
+        self.volume_level = 1
+        self.on = False
 
 # Create method to turn on the TV
+    def turn_on(self):
+        self.on = True
 # Create method to turn off the TV
-
+    def turn_off(self):
+        self.on = False
 # Create method to get the current channel
 # Create method to set the channel within the range of 1 to 120
 # Create method to increase the channel by 1, if the TV is on and the channel is less than 120
